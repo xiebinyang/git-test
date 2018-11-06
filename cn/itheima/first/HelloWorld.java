550719563072551c447315bae0b928c8acf52cc7
xiebinyang <517777254@qq.com>
2018-11-06 15:31:25 +0800

    第一次提交git-test-project2

---
+++ cn/itheima/first/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.itheima.first;
+
+public class HelloWorld {
+
+    public static  void main(String[] args){
+
+        System.out.println("hello 1");
+        System.out.println("hello 2");
+        System.out.println("hello 3");
+        System.out.println("hello 4");
+    }
+}
